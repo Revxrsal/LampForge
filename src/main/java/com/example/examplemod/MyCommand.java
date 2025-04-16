6fbfe6ae23054ebd42bc6d798cab5042d4173028
Revxrsal <aloosh.kasasbeh@gmail.com>
2025-04-16 23:22:00 +0300

    improve the command

--- src/main/java/com/example/examplemod/MyCommand.java
+++ src/main/java/com/example/examplemod/MyCommand.java
@@ -1,13 +1,15 @@
 package com.example.examplemod;
 
 import revxrsal.commands.annotation.Command;
+import revxrsal.commands.annotation.Named;
+import revxrsal.commands.annotation.Range;
 import revxrsal.commands.annotation.Subcommand;
 
 @Command("hello")
 public class MyCommand {
 
     @Subcommand("print")
-    public void print(ForgeCommandActor actor, int times) {
+    public void print(ForgeCommandActor actor, @Named("times") @Range(min = 1, max = 50) int times) {
         for (int i = 0; i < times; i++) {
             actor.sendRawMessage("Hello!!");
         }
